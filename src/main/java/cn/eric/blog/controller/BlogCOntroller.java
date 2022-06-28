4781966f03c82caee62ee32b50a677c77f15e597
eric <2589325627@qq.com>
2022-06-28 08:56:50 +0800

    增加日志配置

--- src/main/java/cn/eric/blog/controller/BlogCOntroller.java
+++ src/main/java/cn/eric/blog/controller/BlogCOntroller.java
@@ -1,5 +1,11 @@
 package cn.eric.blog.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * 控制层
  * 主要实现：
@@ -11,6 +17,12 @@ package cn.eric.blog.controller;
  *         4.2 作者
  *         4.3 修改时间
  */
+@RestController("/")
 public class BlogCOntroller {
-
+    Logger logger = LoggerFactory.getLogger(BlogCOntroller.class);
+    @GetMapping("get")
+    public String getTest(){
+        logger.info("hehe");
+        return "hello,world";
+    }
 }
